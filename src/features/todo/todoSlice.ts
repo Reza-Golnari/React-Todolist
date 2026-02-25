76969e561a419b6f2aca69676f952363997ecc13
r.golnari <r.golnari@raikaco.org>
2026-02-25 18:35:09 +0300

    Add todoSlice to manage todo state with add, remove, and toggle reducers

---
+++ src/features/todo/todoSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState: {
+    todos: [],
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    toggleTodo: (state, action) => {
+      state.todos = state.todos.map((todo) =>
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo,
+      );
+    },
+  },
+});
